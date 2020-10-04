be7f2594dbf3ff293faf533c7936c6b90fff4891
il12 <il5498@yandex.ru>
2020-10-04 23:01:18 +0300

    Added Call for Papers Link on the Main Page; Fixed Background

--- js/generateDates.js
+++ js/generateDates.js
@@ -4,13 +4,12 @@ const generateDates = () => {
         .then(dates=>{
             let datesList = dates.map(item=>`<li><strong>${item.title}:</strong> ${item.date}</li>`)
             let html = `
-                <ul class="list-unstyled">
-                    ${datesList.join('')}
-                </ul>
                 <p> 
                     <strong>All deadlines are for the Moscow time zone, 23:59.</strong>
                 </p>
-
+                <ul class="list-unstyled">
+                    ${datesList.join('')}
+                </ul>
             `
             document.querySelector('.dates').insertAdjacentHTML('afterbegin', html);
         })
